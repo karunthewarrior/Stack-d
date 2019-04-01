d7f08df0e6d265d309b348e666394c3548302ec5
Lukas Merkle <lmerkle@andrew.cmu.edu>
2019-04-01 18:36:12 -0400

    collected data points

--- src/cmu-16662-robot-ctrl/scripts/calibrate_camera.py
+++ src/cmu-16662-robot-ctrl/scripts/calibrate_camera.py
@@ -4,6 +4,7 @@ import rospy
 from ar_track_alvar_msgs.msg import AlvarMarkers
 import kinematics as kin
 import ArmController as controller
+import pickle
 
 
 class camera_calib:
@@ -17,7 +18,7 @@ class camera_calib:
         try:
             ar_position_obj = camera_pose.markers[0].pose.pose.position
             self.ar_position = np.array([ar_position_obj.x,ar_position_obj.y,ar_position_obj.z])
-            rospy.loginfo(self.ar_position)
+            # rospy.loginfo(self.ar_position)
         except:
             self.ar_position = np.array([None,None,None])
 
@@ -47,7 +48,7 @@ if __name__ == "__main__":
     cam = camera_calib()
     controller = controller.ArmController()
     rospy.sleep(2)
-    target_joints = np.deg2rad([[0,-10,20,-70,0],[0,-10,40,-70,0]])
+    target_joints = np.deg2rad([[0,-20,20,-70,0],[0,-20,20,-80,0],[10,-20,30,-80,0],[-20,-20,30,-80,0],[-20,20,30,-80,0],[20,20,20,-80,0],[20,20,0,-70,0],[20,20,0,-70,20],[20,20,0,-50,10],[30,10,0,-50,-20],[40,10,-20,-50,-20],[0,10,-20,-50,0],[0,10,-10,-50,0],[-20,20,30,-80,0]])
     arm_pts_list = []
     cam_pts_list = []
     for joint in target_joints:
@@ -61,6 +62,8 @@ if __name__ == "__main__":
         arm_pts_list.append(arm_pos)
         cam_pts_list.append(cam_pos)
     rospy.loginfo(arm_pts_list)
-    rospy.loginfo(cam_pts_listss)
+    rospy.loginfo(cam_pts_list)
+    pickle.dump(arm_pts_list, open("arm_pts_pos.p","wb"))
+    pickle.dump(cam_pts_list, open("cam_pts_pos.p","wb"))
 
 

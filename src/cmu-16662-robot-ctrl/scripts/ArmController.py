849d1cf31b1df36a507641c2d109e01cebc8c832
Lukas Merkle <lmerkle@andrew.cmu.edu>
2019-04-01 15:45:18 -0400

    created class for calibration

--- src/cmu-16662-robot-ctrl/scripts/ArmController.py
+++ src/cmu-16662-robot-ctrl/scripts/ArmController.py
@@ -33,6 +33,7 @@ class ArmController():
 
     def has_converged(self):
         converged = False
+        rospy.loginfo(np.linalg.norm(self.joint_state-self.joint_target))
         if(np.linalg.norm(self.joint_state-self.joint_target) < 0.1):
             self.history.append(self.time)
             # rospy.loginfo((self.time - self.history[0]).to_sec())
@@ -46,21 +47,23 @@ if __name__ == "__main__":
     rospy.init_node('controller_test', anonymous=True)
     controller = ArmController()
     # target_joints = [[0,0,0,90,0]]
+    target_joints = [[0,10,0,0,0]]
     pos_list = [[0.3,0,0.1],[0.3,0.1,0.15],[0.25,0.1,0.25]]
-    pos_list = []
-    target_joints = [[0.48056895, 0.76073892, 0.02609777, 1.5390177,  0.79548688]]
-    for pos in pos_list:
-        q = kin.inverse_kinematics(pos)
-        if q!=None:
-            target_joints.append(q)
-        else:
-            print("No solution")
-            exit()
-    rospy.sleep(2)
+    # pos_list = []
+    # target_joints = [[0.48056895, 0.76073892, 0.02609777, 1.5390177,  0.79548688]]
+    # for pos in pos_list:
+    #     q = kin.inverse_kinematics(pos)
+    #     if q!=None:
+    #         target_joints.append(q)
+    #     else:
+    #         print("No solution")
+    #         exit()
+    rospy.sleep(1)
     controller.home_arm()
     for joint in target_joints:
         controller.set_joint_state(joint)
         while(not controller.has_converged()):
+            # rospy.loginfo("not")
             pass
 
     # controller.home_arm()
--- src/cmu-16662-robot-ctrl/scripts/calibrate_camera.py
+++ src/cmu-16662-robot-ctrl/scripts/calibrate_camera.py
@@ -1,18 +1,37 @@
 import sys
-
 import numpy as np
 import rospy
-from sensor_msgs.msg import JointState
-from geometry_msgs.msg import 
+from ar_track_alvar_msgs.msg import AlvarMarkers
+import kinematics as kin
+import ArmController as controller
 
-camera_pose_topic = '/ar_pose_marker'
 
-def get_camera_pose(camera_pose):
-    time = joint_state.header.stamp
-    camera_pose = np.array(joint_state.position)[0:5]
-    rospy.loginfo(camera_pose)
+class camera_calib:
+    def __init__(self):
+        self.camera_pose_topic = "/ar_pose_marker"
+        self.ar_position = np.array([None,None,None])
+        sub_tag = rospy.Subscriber(self.camera_pose_topic,AlvarMarkers,self.get_camera_pose)
+
+    def get_camera_pose(self,camera_pose):
+        time = camera_pose.header.stamp
+        try:
+            ar_position_obj = camera_pose.markers[0].pose.pose.position
+            self.ar_position = np.array([ar_position_obj.x,ar_position_obj.y,ar_position_obj.z])
+            rospy.loginfo(self.ar_position)
+        except:
+            self.ar_position = np.array([None,None,None])
 
 if __name__ == "__main__":
-	rospy.init_node('listener', anonymous=True)
-	sub = rospy.Subscriber(camera_pose_topic,JointState,get_joint_state)
-	rospy.spin()
+    rospy.init_node("camera_calib", anonymous=True)
+    cam = camera_calib()
+    controller = controller.ArmController()
+    rospy.sleep(2)
+    target_joints = [[0,-10,20,-70,0]]
+    for joint in target_joints:
+        controller.set_joint_state(joint)
+        while(not controller.has_converged()):
+            pass
+        cam_pos = cam.ar_position
+        arm_pos = kin.forward_kinematics(joint)[0][3,:3]
+        rospy.loginfo(cam_pos)
+        rospy.loginfo(arm_pos)

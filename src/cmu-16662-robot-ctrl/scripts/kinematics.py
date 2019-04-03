2ab75263060806caacb5dde31dbda86a67ff1197
karunthewarrior <karunthewarrior@gmail.com>
2019-04-03 18:42:11 -0400

    Added gripper control

--- src/cmu-16662-robot-ctrl/scripts/kinematics.py
+++ src/cmu-16662-robot-ctrl/scripts/kinematics.py
@@ -1,10 +1,9 @@
 import numpy as np
-# import transforms3d as tf
-import transformations as tf
+import transforms3d as tf
 
 def get_H(orig,axis,angle):
     orig = np.vstack([np.hstack([np.eye(3),np.array(orig).reshape(-1,1)]),[0,0,0,1]])
-    axis = tf.quaternion_matrix(tf.quaternion_about_axis(angle,axis))
+    axis = np.vstack([np.hstack([tf.axangles.axangle2mat(axis, angle),np.array([0, 0, 0]).reshape(-1,1)]),[0,0,0,1]])
     H = np.dot(orig,axis)
     return H 
 
@@ -25,7 +24,7 @@ def forward_kinematics(angles):
         fk_list.append(np.linalg.multi_dot(all_H[0:i]))
     
     centroids = np.vstack([H[:3,-1] for H in fk_list]) 
-    orientation = np.vstack([tf.euler_from_matrix(x,'rxyz') for x in fk_list])
+    orientation = np.vstack([tf.euler.mat2euler(x[:3,:3],'rxyz') for x in fk_list])
 
     pose_mat = np.hstack([centroids,orientation])
     pose = {"joint_1":pose_mat[0],"joint_2":pose_mat[1],"joint_3":pose_mat[2],"joint_4":pose_mat[3],"gripper":pose_mat[4]}
@@ -37,14 +36,13 @@ def jacobian(fk_list):
     for H,axis in zip(fk_list[:3],axis_list[:3]):  #only first three joints
         a = np.dot(H[:3,:3],axis.reshape(-1,1)).reshape(1,-1)
         p = (fk_list[-1][:3,-1] - H[:3,-1]).reshape(1,-1)
-        jac_column = np.vstack([np.cross(a,p).reshape(-1,1),a.reshape(-1,1)])
-        # jac_column = np.cross(a,p).reshape(-1,1)
+        # jac_column = np.vstack([np.cross(a,p).reshape(-1,1),a.reshape(-1,1)])
+        jac_column = np.cross(a,p).reshape(-1,1)
         jac.append(jac_column)
     return np.hstack(jac)
 
 def inverse_kinematics(target_pose,max_iter=1000,offset=True):
     q = np.ones((5,1)) * np.pi/4
-
     gripper_offset = np.array([0, 0, 0.193])
     # print(target_pose,"PRE KARUN")
     if offset:
@@ -56,10 +54,9 @@ def inverse_kinematics(target_pose,max_iter=1000,offset=True):
     q[3] = 0
     q[4] = 0
     x = np.zeros(3)
-
     dx = target_pose - x
     i = 0
-    while(np.any(np.absolute(dx) > 1e-1)):
+    while(np.any(np.absolute(dx) > 1e-4)):
         if i == max_iter:
             print("timeout")
             return None
@@ -69,10 +66,8 @@ def inverse_kinematics(target_pose,max_iter=1000,offset=True):
         dq = np.dot(np.linalg.pinv(jac),dx).reshape(-1,1)
         q[:3] = q[:3] + dq 
         final_pos,_ = forward_kinematics(q)
-
         dx = target_pose - final_pos["joint_4"][:3]
     q[3] = np.pi/2 - final_pos["joint_4"][4] 
-
     q = np.array([map_angle(a) for a in q])
     print(q,"Q",np.rad2deg(q))
     if (np.all(abs(q) >= 0) and np.all(abs(q) <= np.pi/2)):
@@ -95,17 +90,14 @@ def map_angle(a):
 
 if __name__ == "__main__":
     # angles= [-0.9501,0.8786,0.5130,-1.4157,-0.1997]
-    # link_pose,fk_list = forward_kinematics([0,0,0,np.pi,0])
-    # print(link_pose[-1])
+    # link_pose,fk_list = forward_kinematics(angles)
     # jac = jacobian(fk_list)
     # q = inverse_kinematics([0.16557369, -0.23141237,  0.00692751])
-
     pos_list = [[0.3,0,-0.35]]
     for pos in pos_list:
         q = inverse_kinematics(pos)
         print(q,"WQEQWE")
         print("FK",forward_kinematics(q)[0]["joint_4"])
-
         if q !=None:
             print(np.rad2deg(q))
     # print(forward_kinematics(q)[0][-1,0:3])
